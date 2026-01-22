3c97e16ea267148371f3b20e1be4563e4eac7f19
Nausin <nausin.rasulpathan24@gmail.com>
2026-01-22 18:38:15 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args) {
+        // The println function prints the string and adds a newline character at the end.
+        System.out.println("welcome to world");
+    }
+}
+
