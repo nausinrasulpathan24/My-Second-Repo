e6a508774887d8a4af9e276990a85dcb4ca819f0
Nauisn <nausin.rasulpathan24@gmail.com>
2026-02-05 00:17:40 +0530

    Rename class Welcome to hello

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-public class Welcome {
+public class hello {
     public static void main(String[] args) {
         // The println function prints the string and adds a newline character at the end.
         System.out.println("welcome to world");
